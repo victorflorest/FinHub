f29366a56466c131337c907860abbc4257db122e
Victor Flores <gianfranco22.ft@gmail.com>
2026-08-03 13:18:55 -0500

    remove python-decouple

--- financialsite/settings.py
+++ financialsite/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from decouple import config
 import os
 
 import dj_database_url
@@ -30,12 +29,10 @@ SECRET_KEY = os.getenv(
     'django-insecure-186ual4&!4u3d06wmwj=c#s4mfjj2+cywjcngvi&g5955df94('
 )
 
-DEBUG = env_bool('DEBUG', False)
-
-ALLOWED_HOSTS = config(
+ALLOWED_HOSTS = env_list(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost",
-).split(",")
+    "127.0.0.1,localhost"
+)
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv('RENDER_EXTERNAL_HOSTNAME')
 
